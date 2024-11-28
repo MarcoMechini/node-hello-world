4be782ffa9182d2310bf3f1769d3ead0f67bb288
Marco Mechini <mechinimarco35@gmail.com>
2024-11-28 14:02:03 +0100

    feat: bonus 1 complete

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 const terminalWord = process.argv[2]
 console.log(`Hello ${terminalWord}`);
 
+// bonus 1
+const userNumber = parseInt(process.argv[2])
+if (process.argv[2] % 2) {
+    console.log('Dispari');
+} else {
+    console.log('Pari');
+}
+
+// bonus2
